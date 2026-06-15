8d7135d75d0e091d95cd7746c76cef5f71091952
soulsyncverse <soulsync1320@gmail.com>
2026-06-16 01:38:54 +0530

    Update claude.js

--- api/claude.js
+++ api/claude.js
@@ -43,7 +43,7 @@ export default async function handler(req, res) {
   };
 
   const endpoint =
-    `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-05-20:generateContent?key=${apiKey}`;
+  `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${apiKey}`;
 
   try {
     const response = await fetch(endpoint, {
